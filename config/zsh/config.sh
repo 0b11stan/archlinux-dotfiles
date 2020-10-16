1c0df6305c32a7efd77c04c0559c08a646a2cf35
0b11stan <tristan@tic.sh>
2020-10-17 01:12:18 +0200

    feat: configure zshzle

--- config/zsh/config.sh
+++ config/zsh/config.sh
@@ -2,6 +2,9 @@
 export TERM="xterm-256color"
 export EDITOR='nvim'
 export BROWSER=/usr/bin/qutebrowser
+export SAVEHIST=100000
+export HISTSIZE=100000
+export HISTFILE=~/.zsh_history
 
 # enable screen sharing for webrtc
 export XDG_SESSION_TYPE=wayland
@@ -12,3 +15,23 @@ export CMDB_HOST=https://jack.cdweb.biz
 export CMDB_CLIENT_ID=youwhishyouknew
 export CMDB_CLIENT_SECRET=butyoudont
 export REQUESTS_CA_BUNDLE=/etc/ssl/certs/ca-certificates.crt
+
+# for zsh command line editor (see man zshzle)
+bindkey -M vicmd 'j' vi-backward-char
+bindkey -M vicmd 'k' vi-down-line-or-history
+bindkey -M vicmd 'l' vi-up-line-or-history
+bindkey -M vicmd 'm' vi-forward-char
+fzfhistory() {
+  BUFFER=$(history 0 \
+    | cut -c 8- \
+    | fzf --tac \
+          --reverse \
+          --info=hidden \
+          --height=10 \
+          --bind=tab:down \
+          --prompt='| ' \
+  )
+  zle end-of-line
+}
+zle -N fzfhistory fzfhistory
+bindkey -M viins '^R' fzfhistory
--- install.sh
+++ install.sh
@@ -7,16 +7,18 @@ function install_aur_package() {
   } || echo $1 already installed
 }
 
+
 mkdir ~/AUR ~/.config &>/dev/null
 
 sudo pacman -S --noconfirm git neovim python python-pynvim zsh base-devel fzf \
                            clang \
   && install_aur_package neovim-plug \
   && install_aur_package zsh-theme-powerlevel10k-git \
+  && install_aur_package zsh-fast-syntax-highlighting \
   && nvim --headless +PlugInstall +qa
   
-for conf in $(ls config); do
-  ln -s ~/repos/perso/dotfiles/config/$conf ~/.config/$conf &>/dev/null
+for app in $(ls config); do
+  ln -s ~/repos/perso/dotfiles/config/$app ~/.config/$app &>/dev/null
 done
 ln -s ~/repos/perso/dotfiles/p10k.zsh ~/.p10k.zsh &>/dev/null
 ln -s ~/repos/perso/dotfiles/zshrc ~/.zshrc &>/dev/null
