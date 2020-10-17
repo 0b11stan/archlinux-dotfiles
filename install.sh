615cb59e280763329e888618779399e89a82bc11
Tristan Pinaudeau <tristan@tic.sh>
2020-10-17 14:23:03 +0200

    updating aliases

--- install.sh
+++ install.sh
@@ -2,10 +2,14 @@
 
 mkdir ~/AUR ~/.config &>/dev/null
 
-sudo pacman -S --noconfirm git neovim python python-pynvim zsh base-devel fzf \
-                           clang \
+requirements="git neovim python python-pynvim zsh base-devel fzf clang
+              ttf-droid zsh-theme-powerlevel10k rsync"
+
+sudo pacman -S --noconfirm $requirements \
   && ./syncaur.sh \
-  && nvim --headless +PlugInstall +qa
+  && git config --global user.email "tristan@tic.sh" \
+  && git config --global user.name "Tristan Pinaudeau"
+  #&& nvim --headless +PlugInstall +qa \
   
 for app in $(ls config); do
   ln -s ~/repos/perso/dotfiles/config/$app ~/.config/$app &>/dev/null
