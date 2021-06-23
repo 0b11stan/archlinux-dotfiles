cc1ba8185764cf875c4963335181beb1c4341e31
Tristan Pinaudeau <tristan@tic.sh>
2021-06-23 16:34:55 +0200

    refactor using stow

--- install.sh
+++ install.sh
@@ -5,7 +5,7 @@ mkdir ~/AUR ~/.config &>/dev/null
 zshplugins="zsh-autosuggestions zsh-theme-powerlevel10k"
 
 requirements="git neovim python python-pynvim zsh base-devel fzf clang
-              rsync autopep8 ttf-hack"
+              rsync autopep8 ttf-hack stow"
 
 sudo pacman -S --needed --noconfirm $requirements $zshplugins \
   && ./syncaur.sh \
@@ -13,18 +13,6 @@ sudo pacman -S --needed --noconfirm $requirements $zshplugins \
   && git config --global user.name "Tristan Pinaudeau"
   #&& nvim --headless +PlugInstall +qa \
   
-# maybe use stow instead ? http://brandon.invergo.net/news/2012-05-26-using-gnu-stow-to-manage-your-dotfiles.html
-link() { test -L $2 || ln -s $1 $2; }
-
-mkdir -p ~/.config/nvim
-
-for app in $(ls config); do
-  link ~/repos/perso/dotfiles/config/$app ~/.config/$app
-done
-link ~/repos/perso/dotfiles/rcfiles/p10k.zsh ~/.p10k.zsh
-link ~/repos/perso/dotfiles/rcfiles/zshrc ~/.zshrc
-link ~/repos/perso/dotfiles/rcfiles/qute_config.py ~/.config/qutebrowser/config.py
-link ~/repos/perso/dotfiles/rcfiles/nvim_init.vim ~/.config/nvim/init.vim
-link ~/repos/perso/dotfiles/qute_userscripts ~/.local/share/qutebrowser/userscripts
-
 sudo chsh -s /usr/bin/zsh $USER
+
+stow -t /home/tristan/ dotfiles
